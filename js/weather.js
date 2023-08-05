93de99809db6ca735537005af8671f447566cad7
Diego Solis <Endgame189@outlook.com>
2023-08-04 20:45:45 -0500

    Bug fixes
    Specifically the weather thingy

--- js/weather.js
+++ js/weather.js
@@ -1,31 +1,26 @@
 let weather = {
-  // Get your API key from openweathermap.org
-  apikey: "e49ae78c30d6e842a2841907db176af6",
+    "apikey": "e49ae78c30d6e842a2841907db176af6",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=imperial&appid=" +
+            this.apikey)
+            .then((Response) => Response.json())
+            .then((data) => this.displayWeather(data));
+    },
 
-  fetchWeather: function (location) {
-      fetch(
-          "https://api.openweathermap.org/data/2.5/weather?id=" +
-          location +
-          "&units=imperial&appid=" +
-          this.apikey
-      )
-          .then((response) => response.json())
-          .then((data) => this.displayWeather(data));
-  },
+    displayWeather: function (data) {
+        const { icon, description } = data.weather[0];
+        let { temp } = data.main; // Use let here
+        temp = Math.round(temp); // Use Math.round() to round the temperature
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+    },
 
-  displayWeather: function (data) {
-      const { icon, description } = data.weather[0];
-      let { temp } = data.main;
-      temp = Math.round(temp);
-      console.log("Weather Icon URL:", "https://openweathermap.org/img/wn/" + icon + ".png");
-      console.log("Description:", description);
-      console.log("Temperature:", temp + "°F");
-  },
-
-  search: function () {
-      const cityId = document.querySelector(".searchbar").value;
-      this.fetchWeather(cityId);
-  },
+    search: function () {
+        this.fetchWeather(document.querySelector(".searchbar").value);
+    }
 };
 
-weather.fetchWeather("4273857"); // Example: London, United Kingdom (City ID: 2643743)
+weather.fetchWeather("Kansas City");
